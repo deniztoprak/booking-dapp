448d3769214e7d89baf698828c452ac49faa345c
Deniz Toprak <deniztoprak@users.noreply.github.com>
2022-02-05 01:32:15 +0300

    Add initial pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,30 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import { Container, Nav, Navbar } from "react-bootstrap";
 import { AdminPanel } from "./pages/AdminPanel/AdminPanel";
 import { StartPage } from "./pages/StartPage/StartPage";
 
-function App() {
+export function App() {
   return (
-    <Routes>
-      <Route path="/" element={<StartPage />} />
-      <Route path="admin" element={<AdminPanel />} />
-    </Routes>
+    <main>
+      <Navbar bg="primary" variant="dark">
+        <Container fluid>
+          <Link className="navbar-brand" to="/">
+            Booking Dapp
+          </Link>
+          <Nav className="me-auto my-2">
+            <Link className="nav-link" to="/admin">
+              Admin Panel
+            </Link>
+            <Nav.Link href="/admin">Coca Cola</Nav.Link>
+            <Nav.Link href="/admin">Pepsi</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+        <Route path="admin" element={<AdminPanel />} />
+      </Routes>
+    </main>
   );
 }
 
--- client/src/pages/StartPage/StartPage.tsx
+++ client/src/pages/StartPage/StartPage.tsx
@@ -1,3 +1,8 @@
 export function StartPage() {
-  return <div>Start Page</div>;
+  return (
+    <article>
+      <h1 className="text-center mt-5">Welcome to Booking Dapp</h1>
+      <h4 className="text-center">(Please use the top bar to navigate)</h4>
+    </article>
+  );
 }

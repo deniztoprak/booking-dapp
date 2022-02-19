eea80b800ad97ec9bd659dd5d95d5ef9de86d0ae
Deniz Toprak <deniztoprak@users.noreply.github.com>
2022-02-19 12:28:56 +0300

    Finalize employee methods

--- contracts/RoomBooking.sol
+++ contracts/RoomBooking.sol
@@ -5,18 +5,22 @@ import '../client/node_modules/@openzeppelin/contracts/access/Ownable.sol';
 import '../client/node_modules/@openzeppelin/contracts/access/AccessControl.sol';
 
 contract RoomBooking is Ownable, AccessControl {
-    bytes32 public constant COKE_EMPLOYEE = keccak256('COKE_EMPLOYEE');
-    bytes32 public constant PEPSI_EMPLOYEE = keccak256('PEPSI_EMPLOYEE');
+    bytes32 private constant COKE_EMPLOYEE = keccak256('COKE_EMPLOYEE');
+    bytes32 private constant PEPSI_EMPLOYEE = keccak256('PEPSI_EMPLOYEE');
 
     constructor() {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    function addEmployee(bytes32 _role, address _employeeAddress) public {
+    function addEmployee(bytes32 _role, address _employeeAddress) public onlyOwner {
         grantRole(_role, _employeeAddress);
     }
 
-    function removeEmployee(bytes32 _role, address _employeeAddress) public {
+    function removeEmployee(bytes32 _role, address _employeeAddress) public onlyOwner {
         revokeRole(_role, _employeeAddress);
+    }
+
+    function isEmployee(bytes32 _role, address _employeeAddress) public view returns (bool) {
+        return hasRole(_role, _employeeAddress);
     }
 }
